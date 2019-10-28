1de81e64b5cb1e17dcb2e9af07ffb6007ef0f912
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-27 19:21:54 -0700

    added in navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Characters from './Components/Character';
+import Characters from "./Components/Character";
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Characters />
     </div>
   );
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navigation () {
+    return (
+        <div className="nav">
+            <div className="links">
+            </div>
+        </div>
+    )
+}
